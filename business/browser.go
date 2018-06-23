69df433b6918afbf7d08297ca41ad0faa403b761
sunzexin <799416774@qq.com>
2018-06-23 14:20:28 +0800

    update

--- business/browser.go
+++ business/browser.go
@@ -3,24 +3,24 @@ package business
 import (
 	"os/exec"
 	"runtime"
-	"log"
 )
 
-var commands = map[string]string{
-	"windows": "start",
-	"darwin":  "open",
-	"linux":   "xdg-open",
-}
-
-var Version = "0.1.0"
-
 // Open calls the OS default program for uri
-func Open(uri string) error {
-	run, ok := commands[runtime.GOOS]
-	if !ok {
-		log.Println(ok)
-	}
+func Open(url string) error {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd.exe", "/c", "start", url)
+		break
+	case "darwin":
+		cmd = exec.Command("open", url)
+		break
+	case "linux":
+		cmd = exec.Command("xdg-open", url)
+		break
+	default:
+		break
 
-	cmd := exec.Command(run, uri)
+	}
 	return cmd.Start()
 }
